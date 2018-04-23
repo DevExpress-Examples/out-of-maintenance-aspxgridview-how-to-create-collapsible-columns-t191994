a998d8a150f9b0a7a8c52d6930e8914cb895c641
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to create collapsible columns
+
+
+<p>This example demonstrates how to implement collapsible columns. I.e. some columns may be expanded to show additional columns. The main idea is to create two columns: a data column and a band column that contains other data columns and change their visibility during the grid's callback when the expand or collapse buttons are clicked. <br /><br /><strong>See Also:</strong><br /><a href="https://www.devexpress.com/Support/Center/p/T207750">ASPxGridView - How to create collapsible columns (client-side approach)</a></p>
+
+<br/>
+
+
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,41 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+        If Session("ProdNameExpanded") IsNot Nothing Then
+            Dim isExpanded As Boolean = DirectCast(Session("ProdNameExpanded"), Boolean)
+            gridView.Columns("ProdNameBand").Visible = isExpanded
+            gridView.Columns("ProdName").Visible = Not isExpanded
+        End If
+        If Session("ProdPriceExpanded") IsNot Nothing Then
+            Dim isExpanded As Boolean = DirectCast(Session("ProdPriceExpanded"), Boolean)
+            gridView.Columns("ProdPriceBand").Visible = isExpanded
+            gridView.Columns("ProdPrice").Visible = Not isExpanded
+        End If
+    End Sub
+    Protected Sub gridView_CustomCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewCustomCallbackEventArgs)
+        Dim param() As String = e.Parameters.Split("|"c)
+        Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+
+        Dim colName As String = param(1)
+        If param(0) = "collapse" Then
+            grid.Columns(colName & "Band").Visible = False
+            grid.Columns(colName).Visible = True
+            Session(colName & "Expanded") = False
+        ElseIf param(0) = "expand" Then
+            grid.Columns(colName & "Band").Visible = True
+            grid.Columns(colName).Visible = False
+            Session(colName & "Expanded") = True
+        End If
+    End Sub
+
+
+End Class
